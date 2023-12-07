923a174478668077fe9edd763edf867be74feb89
AlexBru78 <alex.bruniaux@gmail.com>
2023-12-07 16:15:32 +0100

    database added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+DELETE FROM student;
+INSERT INTO student (id, name, age) VALUES (1, 'Alex', 60);
+INSERT INTO student (id, name, age) VALUES (2, 'Ethan', 23);
+INSERT INTO student (id, name, age) VALUES (3, 'Pierre', 47);
+INSERT INTO student (id, name, age) VALUES (4, 'Xavier', 35);
+INSERT INTO student (id, name, age) VALUES (5, 'Antoine', 15);
